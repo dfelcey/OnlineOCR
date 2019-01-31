f12ca331f0f10a7bff36861cd7721cd4f6df7a0a
Dave Felcey <dave.felcey@mulesoft.com>
2019-01-31 23:33:11 +0000

    Fixed retries and supports base64 file encoding

--- src/main/java/onlineocr/Utils.java
+++ src/main/java/onlineocr/Utils.java
@@ -6,4 +6,8 @@ public class Utils {
 	public static byte [] Base64Decode(String encodedString) {
 		return Base64.getDecoder().decode(encodedString);
 	}
+
+	public static byte [] Base64Encode(byte [] byteArray) {
+		return Base64.getEncoder().encode(byteArray);
+	}
 }
